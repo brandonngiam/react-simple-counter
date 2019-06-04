a50497c4e353848a309078ac59b7eade5ebec0ee
brandonngiam <brandonngiam@user.noreply.github.com>
2019-06-04 14:32:54 +0800

    Done

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const container = document.querySelector("#app");
 
+function Display(props) {
+  const counter = props.count;
+  return (
+    <div className={`count ${counter < 0 ? " negative" : ""}`}>{counter} </div>
+  );
+}
+
+function Button(props) {
+  return (
+    <button className="button" onClick={props.functionToCall}>
+      {props.text}
+    </button>
+  );
+}
+
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
+    this.state = { count: props.initial };
   }
 
   incrementCount() {
@@ -21,17 +36,31 @@ class Counter extends React.Component {
   render() {
     return (
       <div className="container">
-        <button className="button">{this.state.count}</button>
-        <button className="button" onClick={this.incrementCount.bind(this)}>
-          +
-        </button>
-        <button className="button" onClick={this.decrementCount}>
-          -
-        </button>
+        <Display count={this.state.count} />
+        <Button text="+" functionToCall={this.incrementCount.bind(this)} />
+        <Button text="-" functionToCall={this.decrementCount.bind(this)} />
       </div>
     );
   }
 }
 
-const element = <Counter />;
+function MultipleCounter() {
+  const counters = [
+    { id: "123", initialCount: 0 },
+    { id: "124", initialCount: 1 },
+    { id: "125", initialCount: 2 },
+    { id: "126", initialCount: 3 }
+  ];
+
+  //creates a list of React elements/components
+  const counterItems = counters.map(x => {
+    return <Counter key={x.id} initial={x.initialCount} />;
+  });
+
+  return <React.Fragment>{counterItems}</React.Fragment>;
+}
+
+// const element = <Counter />;
+const element = <MultipleCounter />;
+
 ReactDOM.render(element, container);
